4b4514a9b0f624e61e924019f3d5be9ab039b6d1
Alexandr Yakovlev <123alexher123@gmail.com>
2025-11-25 22:15:10 +0300

    update PracticeFormTests

--- src/test/java/PracticeFormTests.java
+++ src/test/java/PracticeFormTests.java
@@ -36,10 +36,13 @@ public class PracticeFormTests {
         $("#currentAddress").setValue("currentAddress");
 
         $("#state").click();
+        $("#react-select-3-input").setValue("NCR").pressEnter();
 
         $("#city").click();
+        $("#react-select-4-input").setValue("Delhi").pressEnter();
 
+        $("#submit").click();
 
-
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
     }
 }
